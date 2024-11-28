a6810a71b9647bb83442ebba167c6ba2cc47caca
ayden <aydendiel@gmail.com>
2024-11-28 18:26:19 -0500

    feat: created homepage

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,15 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <p className="text-blue-300">Hello, world</p>
-    </>
+    <div className="w-full flex flex-col gap-4 items-center text-slate-300">
+      <h1 className="mt-10 font-extrabold">Fast Web Gallery Maker</h1>
+      <p className="text-lg">Create and host an online 3D gallery of your artwork with nothing but a GitHub account!</p>
+      <div className="border-4 border-dashed border-sky-400 text-sky-400 rounded-lg shadow-lg px-12 py-32 mt-8 hover:border-sky-200 hover:text-sky-200 hover:shadow-2xl transition-colors cursor-pointer">
+        <p className="text-xl">Drag and drop image files, or click to upload.</p>
+      </div>
+    </div>
   )
 }
 
