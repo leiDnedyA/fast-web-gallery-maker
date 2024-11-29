9874c272424629e08398c341d3c7a15b4073e911
ayden <aydendiel@gmail.com>
2024-11-28 19:09:58 -0500

    refactor: homescreen is now step-based with global state

--- components/UploadScreen.tsx
+++ components/UploadScreen.tsx
@@ -48,11 +48,9 @@ export default function UploadScreen({
   };
 
   const removeFile = (index: number): void => {
-    setFiles((prevFiles: UploadedFile) => prevFiles.filter((_, i) => i !== index));
+    setFiles((prevFiles: UploadedFile[]) => prevFiles.filter((_: any, i: number) => i !== index));
   };
   return <div className="w-full flex flex-col gap-4 items-center text-slate-300">
-    <h1 className="mt-10 font-extrabold">Fast Web Gallery Maker</h1>
-    <p className="text-lg">Create and host an online 3D gallery of your artwork with nothing but a GitHub account!</p>
     <div
       className="border-4 border-dashed border-sky-400 text-sky-400 rounded-lg shadow-lg px-12 py-32 mt-8 hover:border-sky-200 hover:text-sky-200 hover:shadow-2xl transition-colors cursor-pointer"
       onDragEnter={handleDrag}
--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,39 @@
-import React, { useState, DragEvent, ChangeEvent } from "react";
+import { useState } from "react";
 import './App.css'
 import UploadScreen from "../components/UploadScreen";
 import { UploadedFile } from "../lib/files";
 
 function App() {
   // Global State
+  const [stepIndex, setStepIndex] = useState(0);
   const [files, setFiles] = useState<UploadedFile[]>([]);
 
   return (
-    <>
-      <UploadScreen files={files} setFiles={setFiles} />
-    </>
+    <div className="w-full flex flex-col gap-4 items-center text-slate-300">
+
+      <h1 className="mt-10 font-extrabold">Fast Web Gallery Maker</h1>
+      <p className="text-lg">Create and host an online 3D gallery of your artwork with nothing but a GitHub account!</p>
+      {
+        stepIndex === 0 ?
+          <UploadScreen files={files} setFiles={setFiles} /> :
+          <p>Step 2</p>
+      }
+      <div className="flex flex-row gap-4 text-white">
+        <button
+          className="mt-6 cursor-pointer bg-sky-600 disabled:bg-sky-600/40 disabled:text-white/40 transition-colors disabled:cursor-default shadow-md"
+          disabled={stepIndex === 0}
+          onClick={() => { setStepIndex((i: number) => i - 1) }}
+        >Last Step</button >
+        <button
+          className="mt-6 cursor-pointer bg-purple-800 disabled:bg-purple-800/40 disabled:text-white/40 transition-colors disabled:cursor-default shadow-md"
+          disabled={
+            (stepIndex === 0 && files.length === 0) ||
+            (stepIndex === 1)
+          }
+          onClick={() => { setStepIndex((i: number) => i + 1) }}
+        >Next Step</button >
+      </div>
+    </div>
   )
 }
 
